80517bf0fb78b55aab08edac510e1ff4198c801f
hackertaco <imsa1224@naver.com>
2020-07-24 10:04:21 +0900

    feat: ⚡util function for refactor

---
+++ src/lib/asyncUtils.js
@@ -0,0 +1,42 @@
+export const createPromiseThunk = (type, promiseCreator) => {
+  const [SUCCESS, ERROR] = [`${type}_SUCCESS`, `${type}_ERROR`];
+  return (param) => async (dispatch) => {
+    dispatch({ type });
+    try {
+      const payload = await promiseCreator(param);
+      dispatch({
+        type: SUCCESS,
+        payload,
+      });
+    } catch (e) {
+      dispatch({
+        type: ERROR,
+        payload: e,
+        error: true,
+        //FSA규칙, Flux Standard Action 꼭 따를 필요는 없으나 따르면 유틸함수 만들기 쉬워진다
+      });
+    }
+  };
+};
+export const reducerUtils = {
+  initial: (data = null) => ({
+    data,
+    loading: false,
+    error: null,
+  }),
+  loading: (prevState = null) => ({
+    data: prevState,
+    loading: true,
+    error: null,
+  }),
+  success: (data) => ({
+    data,
+    loading: false,
+    error: null,
+  }),
+  error: (error) => ({
+    data: null,
+    loading: false,
+    error,
+  }),
+};
