0b8b058f05cde652261f9b2bd58f0f4a6817f452
hackertaco <imsa1224@naver.com>
2020-07-24 19:23:17 +0900

    refactor: 🔧problem about id value solved

--- src/modules/posts.js
+++ src/modules/posts.js
@@ -3,6 +3,8 @@ import {
   reducerUtils,
   createPromiseThunk,
   handleAsyncActions,
+  createPromiseThunkById,
+  handleAsyncActionsById,
 } from "../lib/asyncUtils";
 
 const GET_POSTS = "GET_POSTS";
@@ -13,17 +15,67 @@ const GET_POST = "GET_POST";
 const GET_POST_SUCCESS = "GET_POST_SUCCESS";
 const GET_POST_ERROR = "GET_POST_ERROR";
 
+const CLEAR_POST = "CLEAR_POST";
+
 //thunk 함수
 //따로 액션생성함수는 안만들었당
 export const getPosts = createPromiseThunk(GET_POSTS, postsAPI.getPosts);
-export const getPost = createPromiseThunk(GET_POST, postsAPI.getPostById);
+export const getPost = createPromiseThunkById(GET_POST, postsAPI.getPostById);
 
+// (id) => async (dispatch) => {
+//   dispatch({ type: GET_POST, meta: id });
+//   try {
+//     const payload = await postsAPI.getPostById(id);
+//     dispatch({ type: GET_POST_SUCCESS, payload, meta: id });
+//   } catch (e) {
+//     dispatch({
+//       type: GET_POST_ERROR,
+//       payload: e,
+//       meta: id,
+//       error: true,
+//     });
+//   }
+// };
+export const clearPost = () => ({ type: CLEAR_POST });
 const initialState = {
   posts: reducerUtils.initial(),
-  post: reducerUtils.initial(),
+  post: {},
 };
-const getPostsReducer = handleAsyncActions(GET_POSTS, "posts");
-const getPostReducer = handleAsyncActions(GET_POST, "post");
+const getPostsReducer = handleAsyncActions(GET_POSTS, "posts", true);
+const getPostReducer = handleAsyncActionsById(GET_POST, "post", true);
+
+// (state, action) => {
+//   const id = action.meta;
+//   switch (action.type) {
+//     case GET_POST:
+//       return {
+//         ...state,
+//         post: {
+//           ...state.post,
+//           [id]: reducerUtils.loading(state.post[id] && state.post[id].data),
+//         },
+//       };
+//     case GET_POST_SUCCESS:
+//       return {
+//         ...state,
+//         post: {
+//           ...state.post,
+//           [id]: reducerUtils.success(action.payload),
+//         },
+//       };
+//     case GET_POST_ERROR:
+//       return {
+//         ...state,
+//         post: {
+//           ...state.post,
+//           [id]: reducerUtils.error(action.payload),
+//         },
+//       };
+//     default:
+//       return state;
+//   }
+// };
+
 export default function posts(state = initialState, action) {
   switch (action.type) {
     case GET_POSTS:
@@ -34,6 +86,11 @@ export default function posts(state = initialState, action) {
     case GET_POST_SUCCESS:
     case GET_POST_ERROR:
       return getPostReducer(state, action);
+    case CLEAR_POST:
+      return {
+        ...state,
+        post: reducerUtils.initial(),
+      };
     default:
       return state;
   }
